1b4b034dcc4120813f4db81e386228ac407315c6
KP <42572567+kpatel122@users.noreply.github.com>
2022-08-19 11:23:58 +0100

    initial merge of screen control and kart control on stm32 controller

--- stm32_control/STM32_Control.ino
+++ stm32_control/STM32_Control.ino
@@ -33,6 +33,16 @@ MCUFRIEND_kbv tft;
 #define GEAR_SHIFT_UP_PIN PB12
 #define GEAR_SHIFT_DOWN_PIN PB13
 
+/*buttons*/
+#define ENGINE_START_BUTTON_PIN PA9
+
+#define THROTTLE_PIN PB14
+#define THROTTLE_PRESSED LOW //throttle switch is active low
+
+#define MOTOR_DRIVER_DIR_PIN PB15
+#define MOTOR_DRIVER_PWM_PIN PA8
+
+/* Gears */
 enum GEAR
 {
   GEAR_REVERSE = 0,
@@ -50,7 +60,7 @@ enum GEAR_SHIFT
 };
 
 GEAR_SHIFT NextGearShift = GEAR_SHIFT_NONE;
-GEAR CurrentGear = GEAR_NEUTRAL;
+GEAR CurrentGear = GEAR_FIRST;//GEAR_NEUTRAL;
 GEAR NewGear = GEAR_NEUTRAL;
 
 /*Serial*/
@@ -60,6 +70,68 @@ GEAR NewGear = GEAR_NEUTRAL;
 DFRobotDFPlayerMini DFPlayer;
 #define DFPLAYER_BAUD 9600
 bool IsEngineSamplePlaying = false;
+
+
+/*Throttle ramp control */
+/* min speed parameters- soft start motor ramping */
+const byte  RAMP_FORWARD_END_SPEED  = 150; //max 255
+
+const byte RAMP_FORWARD_START_SPEED = 120;
+const byte  RAMP_FORWARD_TIME_SECONDS = 2; // how long it takes to get to max speed
+
+const byte  RAMP_REVERSE_END_SPEED = 150;
+
+
+const byte  RAMP_REVERSE_START_SPEED = 120;
+const byte  RAMP_REVERSE_TIME_SECONDS = 2;
+
+//stop instantly would cause a jerk- smooth stop instead
+ #define  RAMP_DECELERATE_TIME 1000 //how quickly we should stop- TODO needs to be a % of our speed
+
+byte RampStartSpeed = RAMP_FORWARD_START_SPEED;
+byte RampEndSpeed = RAMP_FORWARD_END_SPEED;
+byte RampTimeSeconds = RAMP_FORWARD_TIME_SECONDS;
+
+enum RAMP_STATE
+{
+	RAMP_NOT_READY,
+	RAMP_READY,
+	RAMP_STARTED_DECELERATING,
+	RAMP_STARTED_ACCELERATING,
+	RAMP_STOPPED,
+	RAMP_ACCELERATING,
+	RAMP_DECELERATING,
+	RAMP_FINISHED_ACCELERATING,
+	RAMP_FINISHED_DECELERATING
+};
+
+volatile RAMP_STATE RampState = RAMP_FINISHED_DECELERATING; //gets changed in ISR
+
+
+/* kart state machine */
+enum KART_STATE
+{
+  STATE_OFF,
+  STATE_IGNITION_START,
+  STATE_READY,
+  STATE_RAMPING,
+  STATE_MOVING_FORWARD,
+  STATE_MOVING_REVERSE,
+  STATE_NOT_MOVING
+};
+
+volatile KART_STATE CurrState = STATE_OFF;
+KART_STATE PrevState = STATE_OFF;
+
+//stores the current speed of the motor
+int CurrMotorSpeed = 0;
+
+//Motor must be wired in the following way for these to be valid:
+//MOTOR->CRYTON DRIVER
+//MOTOR RED LEAD->MA
+//MOTOR BLACK LEAD->MB
+#define MOTOR_FORWARD HIGH
+#define MOTOR_BACKWARD LOW
 
 
 //debug LED blink
@@ -82,22 +154,18 @@ void InitSoundPlayer()
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
-    blink(10,500);
+    //blink(10,500);
   }
   else
   {
-    blink(5,1000);
+    //blink(5,1000);
     Serial.println(F("DFPlayer Mini online."));
      
-  }
-   
-  
-    
-  
+  }  
 }
 
 
-void InitControlPins()
+void InitGearShiftPins()
 {
   pinMode(GEAR_SHIFT_UP_PIN, INPUT_PULLUP);
   pinMode(GEAR_SHIFT_DOWN_PIN, INPUT_PULLUP);
@@ -178,31 +246,179 @@ void ProcessGearShift()
   }
 }
 
+void InitScreen()
+{
+    uint16_t ID = tft.readID();
+    //Serial.println("Example: Font_simple");
+    //Serial.print("found ID = 0x");
+    //Serial.println(ID, HEX);
+    if (ID == 0xD3D3) ID = 0x9481; //force ID if write-only display
+    tft.begin(ID);
+    tft.setRotation(1);
+}
+
+void StartScreen()
+{
+  tft.fillScreen(TFT_BLACK);
+  //showmsgXY(210, 200, 4, &FreeSansBold24pt7b, "N",TFT_WHITE);
+  showmsgXY(210, 200, 4, &FreeSansBold24pt7b, "L",TFT_BLACK);
+}
+
+void InitMotorPins()
+{
+   //cryton module
+  pinMode(MOTOR_DRIVER_PWM_PIN,OUTPUT);
+  digitalWrite(MOTOR_DRIVER_PWM_PIN,LOW);
+  pinMode(MOTOR_DRIVER_DIR_PIN,OUTPUT);
+  digitalWrite(MOTOR_DRIVER_DIR_PIN,LOW);
+}
+
+void ThrottleReleasedISR()
+{
+	if(RampState != RAMP_DECELERATING )//make sure we are not already decelarating
+	{
+		RampState = RAMP_STARTED_DECELERATING;//ramp down the speed
+	}
+}
+
+void MotorForward()
+{
+	analogWrite(MOTOR_DRIVER_PWM_PIN,CurrMotorSpeed);
+}
+
+void MotorEnable()
+{
+	digitalWrite(MOTOR_DRIVER_DIR_PIN,HIGH);
+}
+
+void InitISR()
+{
+  pinMode(THROTTLE_PIN,INPUT_PULLUP); 
+	//initialise interrupts
+	attachInterrupt(digitalPinToInterrupt(THROTTLE_PIN), ThrottleReleasedISR, RISING);
+//    attachInterrupt(digitalPinToInterrupt(ENGINE_START_BUTTON_PIN), EngineStartButtonISR, FALLING); 
+}
+
+void ProcessRamp()
+{
+	static unsigned long starttime; //when the ramp was started
+	unsigned long timedelta; //time difference from when the ramp was started 
+	static long resolutioncounter = 0; //resolution of ramp
+
+	switch(RampState)
+	{
+		case RAMP_DECELERATING:
+		{
+
+			//get the current delta from when the decelarion was started
+			timedelta = millis() - starttime;
+
+			//motor speed is mapped against the decelation time- TODO make this is percentage
+			//of current speed
+			CurrMotorSpeed = map(timedelta, 0, RAMP_DECELERATE_TIME, RampStartSpeed, 0);
+
+			//continue to move at the slower decelaration speed to avoif sudden jerky stop
+			MotorForward();
+						
+			//cehck if decelaration has finished
+			if(CurrMotorSpeed <= 0)
+			{
+				RampState = RAMP_FINISHED_DECELERATING;
+			}
+		}break;
+		case RAMP_ACCELERATING:
+		{
+			/*
+			Serial.print("RampEndSpeed ");
+			Serial.print(RampEndSpeed);
+			Serial.print(" CurrMotorSpeed ");
+			Serial.println(CurrMotorSpeed);
+*/
+			//this could be going forward or backward, the speed profiles for either is set SetRampProfile
+			//called in the gear change
+			
+			//get time delta from current time to when the throttle was pressed
+			timedelta = millis() - starttime;
+			CurrMotorSpeed = map(timedelta, 0, RampTimeSeconds*1000, RampStartSpeed, RampEndSpeed);
+
+			MotorForward();
+
+			if(CurrMotorSpeed >= RampEndSpeed)
+			{
+				//Serial.println("***********Ramp Complete******** ");
+				RampState = RAMP_FINISHED_ACCELERATING;
+			}
+
+		}break;
+
+		case RAMP_NOT_READY:
+		{
+
+		}break;
+		case RAMP_READY:
+		{
+
+		}break;
+		case RAMP_STARTED_ACCELERATING:
+		{
+			 
+			RampState = RAMP_ACCELERATING;
+			starttime = millis();
+			resolutioncounter = 0;
+			timedelta = 0;
+			 
+			RampStartSpeed = CurrMotorSpeed;
+			MotorForward();//pMotorMove();
+
+
+		}break;
+		case RAMP_STARTED_DECELERATING:
+		{
+			//setup decelarating values
+			RampState = RAMP_DECELERATING;
+			starttime = millis();
+			resolutioncounter = 0;
+			timedelta = 0;
+			RampStartSpeed = CurrMotorSpeed;
+
+		}break;
+		case RAMP_STOPPED:
+		{
+
+		}break;
+		case RAMP_FINISHED_DECELERATING:
+		case RAMP_FINISHED_ACCELERATING:
+		{
+
+
+		}break;
+	}
+
+}
+
 void setup(void)
 {
     Serial.begin(BAUD);
 
     
 
-    InitControlPins();
+    InitGearShiftPins();
 
-    uint16_t ID = tft.readID();
-    Serial.println("Example: Font_simple");
-    Serial.print("found ID = 0x");
-    Serial.println(ID, HEX);
-    if (ID == 0xD3D3) ID = 0x9481; //force ID if write-only display
-    tft.begin(ID);
-    tft.setRotation(1);
-
+    
+    InitScreen();
     pinMode(LED_BUILTIN, OUTPUT);
     
     digitalWrite(LED_BUILTIN,HIGH); //active low
 
     InitSoundPlayer();
+    InitMotorPins();
+    InitISR();
 
-    tft.fillScreen(TFT_BLACK);
-
-    showmsgXY(210, 200, 4, &FreeSansBold24pt7b, "N",TFT_WHITE);
+    blink(3,100);
+}
+inline int CheckThrottle()
+{
+	return digitalRead(THROTTLE_PIN) ;
 }
 
 String IncomingSerialString = "";
@@ -214,6 +430,16 @@ void loop(void)
 	{
 		ProcessGearShift();
 	}
+
+  //throttle pressed and we have stopped
+  if(CheckThrottle() == THROTTLE_PRESSED && (RampState == RAMP_FINISHED_DECELERATING)) //&& (CurrState!= STATE_OFF))
+  {
+	  RampState = RAMP_STARTED_ACCELERATING;
+
+    showmsgXY(100, 100, 4, &FreeSansBold24pt7b, "ST",TFT_RED);
+  }
+
+  ProcessRamp(); //process movement
   
 }
 
